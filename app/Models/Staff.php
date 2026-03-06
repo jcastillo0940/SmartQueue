81af1d805e80a4ff6a34ebd5e9790022302bc3fe
jcastillo0940 <jeremy.castillo@supercarnes.com>
2026-03-06 15:37:02 -0500

    Integrate HasTenant trait and add tenant_id field
    Added the HasTenant trait and included tenant_id in fillable properties.

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -2,14 +2,17 @@
 
 namespace App\Models;
 
-use App\Models\Concerns\HasTenant;
+use App\Models\Concerns\HasTenant; // 1. Importar el trait
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Staff extends Model
 {
+    use HasTenant; // 2. Usar el trait
+
     protected $fillable = [
+        'tenant_id', // 3. Añadir tenant_id
         'department_id',
         'name',
         'qr_hash',
